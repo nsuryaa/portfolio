d56070876c9e9aee10c5dbc5e38d858fadb0d79d
Surya <suryasedulous@gmail.com>
2024-01-07 20:17:32 +0530

    readme updated

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -0,0 +1,13 @@
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": () => import("./../../../node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx?export=default" /* webpackChunkName: "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx" */),
+  "component---src-pages-404-tsx": () => import("./../../../src/pages/404.tsx?export=default" /* webpackChunkName: "component---src-pages-404-tsx" */)
+}
+
+
+
+exports.head = {
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": () => import("./../../../node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx?export=head" /* webpackChunkName: "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsxhead" */),
+  "component---src-pages-404-tsx": () => import("./../../../src/pages/404.tsx?export=head" /* webpackChunkName: "component---src-pages-404-tsxhead" */)
+}
+
---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/maxy/Documents/Projects/portfolio/.cache/dev-404-page.js")),
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": preferDefault(require("/home/maxy/Documents/Projects/portfolio/node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx")),
+  "component---src-pages-404-tsx": preferDefault(require("/home/maxy/Documents/Projects/portfolio/src/pages/404.tsx"))
+}
+
